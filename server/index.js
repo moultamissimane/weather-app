9a2ad086e8b159c39cd5d594c3f75dc93ee018a0
moultamissimane <93978575+moultamissimane@users.noreply.github.com>
2023-01-27 12:30:54 +0100

    add server folder to repo

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+const port = 3000;
+
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
