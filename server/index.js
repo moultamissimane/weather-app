45f35888a4cfa625e9e7ee135ea1ee0a8a833060
moultamissimane <93978575+moultamissimane@users.noreply.github.com>
2023-01-29 18:08:06 +0100

    edit index

--- server/index.js
+++ server/index.js
@@ -1,15 +1,22 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const PORT = process.env.PORT || 5000;
+const connectDB = require("./Db");
+require("./models/User");
+const AuthRoutes = require("./Routes/AuthRoutes");
 
-const port = 3000;
+connectDB();
 
 app.use(bodyParser.json());
 
+app.use(AuthRoutes);
+
 app.get("/", (req, res) => {
+  console.log(req.body);
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`listening on ${PORT} successfully ✨`);
 });
