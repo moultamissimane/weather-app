e2ff15a1f409520a36e1e1d0f483b1908c9fd961
moultamissimane <93978575+moultamissimane@users.noreply.github.com>
2023-02-08 21:52:04 +0100

    update home screen

--- my-app/src/screens/Home.jsx
+++ my-app/src/screens/Home.jsx
@@ -1,6 +1,7 @@
 import {
   Dimensions,
   ImageBackground,
+  SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
@@ -8,7 +9,7 @@ import {
   useWindowDimensions,
   View,
 } from "react-native";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Night from "../../assets/night.jpg";
 import Rain from "../../assets/rain.jpg";
 import { MaterialIcons } from "@expo/vector-icons";
@@ -24,195 +25,30 @@ import DailyData from "../components/DailyData";
 
 const Home = () => {
   const { height, width } = Dimensions.get("window");
+  const [Full_Date, setFull_Date] = useState("");
+  const [weatherData, setweatherData] = useState(null);
+  const [tempMode, setTempMode] = useState("C");
 
-  const date = new Date();
-  const Full_Date = date.toDateString();
+  useEffect(() => {
+    Date = new Date();
+    date =
+      Date.getDate() + "/" + (Date.getMonth() + 1) + "/" + Date.getFullYear();
+    setFull_Date(date);
+  }, []);
 
-  // const { tempMode, weatherData } = useTemp();
+  useEffect(() => {
+    console.log("weatherData");
+  }, []);
 
-  const { tempMode, weatherData } = useTemp();
-
-  if (weatherData) {
-    const { temp, humidity, wind_speed, weather, pressure, dt } =
-      weatherData.current;
-    const { daily } = weatherData;
-    const date = new Date();
-    const hour = date.getHours();
-    const { main } = weather[0];
-    const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-    const daysData = [];
-    const tempData = [];
-
-    {
-      daily.map((e, idx) => {
-        if (idx >= 1) {
-          const dd = new Date(e.dt * 1000).getUTCDay();
-          daysData.push(days[dd]);
-          tempData.push(e.temp["day"]);
-        }
-      });
-    }
-
-    return (
-      <View style={styles.main}>
-        <StatusBar style="inverted" />
-
+  return (
+    <View style={styles.main}>
+      <StatusBar style="inverted" />
+      <SafeAreaView>
+        <Text style={styles.dateText}>{Full_Date}</Text>
         {/* Present Date */}
-        <View style={styles.date}>
-          <Text style={styles.dateText}>{Full_Date}</Text>
-        </View>
-
-        {/* Current Location */}
-        <View style={styles.location}>
-          <Text style={styles.locationText}>Today</Text>
-        </View>
-
-        {/*Weather Icon */}
-        <View style={[styles.weatherIconView]}>
-          {main === "Haze" ? (
-            <Image
-              style={{ height: 120, width: 160 }}
-              source={require(`../../assets/weatherIcons/Haze.png`)}
-            />
-          ) : null}
-          {main === "Rain" ? (
-            <Image
-              style={{ height: 160, width: 160 }}
-              source={require(`../../assets/weatherIcons/Rain.png`)}
-            />
-          ) : null}
-          {main === "Snow" ? (
-            <Image
-              style={{ height: 130, width: 160 }}
-              source={require(`../../assets/weatherIcons/SnowFall.png`)}
-            />
-          ) : null}
-          {main === "Thunderstorm" ? (
-            <Image
-              style={{ height: 160, width: 160 }}
-              source={require(`../../assets/weatherIcons/ThunderStorm.png`)}
-            />
-          ) : null}
-
-          {/*Drizzle Weather */}
-          {main === "Drizzle" && hour < 19 ? (
-            <Image
-              style={{ height: 160, width: 160 }}
-              source={require(`../../assets/weatherIcons/Drizzle.png`)}
-            />
-          ) : null}
-          {main === "Drizzle" && hour >= 19 ? (
-            <Image
-              style={{ height: 160, width: 160 }}
-              source={require(`../../assets/weatherIcons/Night_Drizzle.png`)}
-            />
-          ) : null}
-
-          {/*Mist Weather */}
-          {main === "Mist" && hour < 19 ? (
-            <Image
-              style={{ height: 130, width: 170 }}
-              source={require(`../../assets/weatherIcons/Mist.png`)}
-            />
-          ) : null}
-          {main === "Mist" && hour >= 19 ? (
-            <Image
-              style={{ height: 150, width: 150 }}
-              source={require(`../../assets/weatherIcons/Night_Mist.png`)}
-            />
-          ) : null}
-
-          {/*Cloudy Weather */}
-          {main === "Clouds" && hour < 19 ? (
-            <Image
-              style={{ height: 130, width: 170 }}
-              source={require(`../../assets/weatherIcons/Cloudy.png`)}
-            />
-          ) : null}
-          {main === "Clouds" && hour >= 19 ? (
-            <Image
-              style={{ height: 160, width: 160 }}
-              source={require(`../../assets/weatherIcons/Night_Cloudy.png`)}
-            />
-          ) : null}
-
-          {/*Clear Weather */}
-          {main === "Clear" && hour < 19 ? (
-            <Image
-              style={{ height: 160, width: 160 }}
-              source={require(`../../assets/weatherIcons/Sunny.png`)}
-            />
-          ) : null}
-          {main === "Clear" && hour >= 19 ? (
-            <Image
-              style={{ height: 150, width: 160 }}
-              source={require(`../../assets/weatherIcons/Night_Clear.png`)}
-            />
-          ) : null}
-        </View>
-
-        {/*Temprature */}
-        <View>
-          <Text style={styles.tempText}>
-            {parseInt(temp)}
-            <Text style={styles.tempmodeText}>{tempMode ? "°F" : "°C"}</Text>
-          </Text>
-        </View>
-
-        {/*Weather Condition */}
-        <View>
-          <Text style={styles.weatherState}>{main}</Text>
-        </View>
-
-        {/*Other Weather Data */}
-
-        <View style={styles.otherData}>
-          <View style={styles.Humidity}>
-            <MaterialCommunityIcons
-              name="water-outline"
-              size={36}
-              color="rgba(256,256,256,0.9)"
-            />
-            <Text style={styles.otherDataValueText}>
-              {humidity} <Text style={styles.unitText}>%</Text>
-            </Text>
-            <Text style={styles.otherDataText}>Humidity</Text>
-          </View>
-          <View style={styles.Pressure}>
-            <MaterialCommunityIcons
-              name="weather-windy"
-              size={36}
-              color="rgba(256,256,256,0.9)"
-            />
-            <Text style={styles.otherDataValueText}>
-              {wind_speed} <Text style={styles.unitText}>km/h</Text>
-            </Text>
-            <Text style={styles.otherDataText}>Wind</Text>
-          </View>
-          <View style={styles.WindSpeed}>
-            <MaterialCommunityIcons
-              name="weather-pouring"
-              size={36}
-              color="rgba(256,256,256,0.9)"
-            />
-            <Text style={styles.otherDataValueText}>
-              {pressure} <Text style={styles.unitText}>hPa</Text>
-            </Text>
-            <Text style={styles.otherDataText}>pressure</Text>
-          </View>
-        </View>
-
-        {/*7 Day Weather Graph */}
-
-        <View style={styles.DailyData}>
-          <DailyData dayData={daysData} tempData={tempData} />
-        </View>
-      </View>
-    );
-  } 
-  else {
-    return <Loading />;
-  }
+      </SafeAreaView>
+    </View>
+  );
 };
 
 export default Home;
@@ -220,14 +56,14 @@ export default Home;
 const styles = StyleSheet.create({
   main: {
     flex: 1,
-    backgroundColor: BACKGROUND_COLOR,
+    backgroundColor: "black",
   },
   date: {
     marginTop: "15%",
     marginLeft: "7%",
   },
   dateText: {
-    color: "rgba(256,256,256,0.63)",
+    color: "rgba(255,255,255,0.63)",
     fontSize: 12,
   },
   location: {
@@ -238,7 +74,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   locationText: {
-    color: "rgba(256,256,256,0.9)",
+    color: "rgba(255,255,255,0.9)",
     fontSize: 25,
     fontWeight: "bold",
     marginLeft: 4,
@@ -250,15 +86,15 @@ const styles = StyleSheet.create({
     marginVertical: 30,
   },
   tempText: {
-    color: "rgba(256,256,256,0.9)",
+    color: "rgba(255,255,255,0.9)",
     fontSize: 60,
     alignSelf: "center",
   },
   tempmodeText: {
-    color: "rgba(256,256,256,0.4)",
+    color: "rgba(255,255,255,0.4)",
   },
   weatherState: {
-    color: "rgba(256,256,256,0.55)",
+    color: "rgba(255,255,255,0.55)",
     fontSize: 16,
     alignSelf: "center",
     textTransform: "uppercase",
@@ -307,17 +143,17 @@ const styles = StyleSheet.create({
   },
   otherDataValueText: {
     fontSize: 14,
-    color: "rgba(256,256,256,0.9)",
+    color: "rgba(255,255,255,0.9)",
   },
   otherDataText: {
     fontSize: 14,
-    color: "rgba(256,256,256,0.55)",
+    color: "rgba(255,255,255,0.55)",
     marginTop: 10,
     textTransform: "capitalize",
   },
   unitText: {
     fontSize: 11,
-    color: "rgba(256,256,256,0.55)",
+    color: "rgba(255,255,255,0.55)",
   },
   DailyData: {
     flex: 1,
